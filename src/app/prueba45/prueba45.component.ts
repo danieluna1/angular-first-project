ccfc42dac31964c0cd041ce74fbf9b8a8f672114
danieluna1 <daniel.luna@nube.sep.gob.mx>
2022-06-28 21:10:38 -0500

    2 files modified, 4 added

---
+++ src/app/prueba45/prueba45.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prueba45',
+  templateUrl: './prueba45.component.html',
+  styleUrls: ['./prueba45.component.css']
+})
+export class Prueba45Component implements OnInit {
+
+today = new Date();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
